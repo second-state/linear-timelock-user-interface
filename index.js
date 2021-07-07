a8b8a84c923103da2ba75310a8d3569f40d1f97f
tpmccallum <mistermac2008@gmail.com>
2021-07-07 15:29:33 +1000

    Change served file

---
+++ index.js
@@ -0,0 +1,31 @@
+// index.js
+
+/**
+ * Required External Modules
+ */
+const express = require("express");
+const path = require("path");
+
+/**
+ * App Variables
+ */
+const app = express();
+const port = process.env.PORT || "8000";
+
+/**
+ *  App Configuration
+ */
+
+/**
+ * Routes Definitions
+ */
+app.get("/", (req, res) => {
+  res.status(200).send("Faucet: Social Media Authenticated Faucet");
+});
+
+/**
+ * Server Activation
+ */
+app.listen(port, () => {
+  console.log(`Listening to requests on http://localhost:${port}`);
+});
