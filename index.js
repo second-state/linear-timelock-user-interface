1b9db60daf62df58481befb95ac05cbf37ecf5c9
tpmccallum <mistermac2008@gmail.com>
2021-07-08 12:22:24 +1000

    Adding packages required

--- index.js
+++ index.js
@@ -5,24 +5,31 @@
  */
 const express = require("express");
 const path = require("path");
+require('dotenv').config();
+var cors = require('cors');
 
 /**
  * App Variables
  */
 const app = express();
 const port = process.env.PORT || "8000";
+const xxxx = process.env.xxxx;
+
 
 /**
  *  App Configuration
  */
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "/public/")));
+app.use(cors());
 
 /**
  * Routes Definitions
  */
 app.get("/", (req, res) => {
-  res.status(200).send("Faucet: Social Media Authenticated Faucet");
+  res.render("index", { title: "Home" });
 });
-
 /**
  * Server Activation
  */
