4e71e1d6b2a0a11a1a04576e3952be6222927c3e
tpmccallum <mistermac2008@gmail.com>
2021-08-31 09:38:54 +1000

    Faucetbot

--- index.js
+++ index.js
@@ -528,9 +528,21 @@ async function seeWhoFollows() {
 /**
  * Telegram Bot 
 **/
+console.log("Starting Telegram Bot");
 const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.telegram_bot_token;
 const bot = new TelegramBot(token, {polling: true});
+bot.onText(/\/faucet (.+)/, (msg, match) => {
+    // The user's id who sent the command
+    //const chatId = msg.chat.id
+
+    // The message which they sent
+    const resp = match[1]
+    console.log("Message recieved: " + resp);
+
+    // Optional message to send back to original user
+    //bot.sendMessage(chatId, resp)
+})
 
 /**
  * Server Activation
