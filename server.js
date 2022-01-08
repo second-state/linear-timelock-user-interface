7a836897c6598b19fd407879998f7efbb190811a
tpmccallum <mistermac2008@gmail.com>
2022-01-08 22:50:21 +1000

    Publish using gh-pages npm package

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve("public/index.html"));
+    res.sendFile(path.resolve("public/html/index.html"));
 });
 
 app.listen(80, () => console.log("Server running..."));
